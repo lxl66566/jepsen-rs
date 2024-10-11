94fe284dc73649ea6c6eec041f75ff68a5bf758b
lxl66566 <lxl66566@gmail.com>
2024-10-11 09:02:39 +0800

    feat(generator): implement Generator
    Signed-off-by: lxl66566 <lxl66566@gmail.com>

--- src/generator/context.rs
+++ src/generator/context.rs
@@ -13,6 +13,65 @@ use crate::{
 
 type IdSetType = Arc<Mutex<BTreeSet<u64>>>;
 
+/// The id allocator. Each thread will use different id.
+#[derive(Clone, Debug)]
+pub struct GeneratorId {
+    id: u64,
+    id_set: IdSetType,
+}
+
+impl GeneratorId {
+    /// Create a new generator id
+    pub fn new(id_set: IdSetType) -> Self {
+        Self {
+            id: Self::alloc_id(&id_set),
+            id_set,
+        }
+    }
+
+    /// Get the id
+    pub fn get(&self) -> u64 {
+        self.id
+    }
+
+    /// Find the minimal usable id in the thread pool
+    fn get_next_id(id_set: &IdSetType) -> u64 {
+        let pool = id_set.lock().expect("Failed to lock thread pool");
+        for (index, id) in pool.iter().enumerate() {
+            if index as u64 != *id {
+                return index as u64;
+            }
+        }
+        pool.len() as u64
+    }
+
+    /// Allocate a new generator id, get a new [`NodeHandle`] from client and
+    /// assoc with this id.
+    fn alloc_id(id_set: &IdSetType) -> u64 {
+        let id = Self::get_next_id(id_set);
+        let res = id_set
+            .lock()
+            .expect("Failed to lock thread pool")
+            .insert(id);
+        debug_assert!(res, "insert must be success");
+        id
+    }
+    /// Free the generator id and it's handle.
+    fn free_id(&self, id: u64) -> bool {
+        self.id_set
+            .lock()
+            .expect("Failed to lock thread pool")
+            .remove(&id)
+    }
+}
+
+impl Drop for GeneratorId {
+    fn drop(&mut self) {
+        let res = self.free_id(self.id);
+        debug_assert!(res, "free must be success");
+    }
+}
+
 /// The global context
 #[non_exhaustive]
 pub struct Global<'a, T: Send = Op, ERR: Send = ErrorType> {
@@ -43,6 +102,11 @@ impl<'a, T: Send + 'a, ERR: Send> Global<'a, T, ERR> {
         }
     }
 
+    /// Alloc a new generator id
+    pub fn get_id(&self) -> GeneratorId {
+        GeneratorId::new(Arc::clone(&self.id_set))
+    }
+
     /// Take the next `n` ops from the raw generator.
     pub fn take_seq(&self, n: usize) -> Vec<T> {
         if let Some(gen) = self.gen.lock().expect("Failed to lock gen").as_mut() {
@@ -50,5 +114,25 @@ impl<'a, T: Send + 'a, ERR: Send> Global<'a, T, ERR> {
         } else {
             Vec::new()
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_alloc_and_free_id() {
+        let id_set = Arc::new(Mutex::new(BTreeSet::new()));
+        let id0 = GeneratorId::new(id_set.clone());
+        assert_eq!(id0.get(), 0);
+        let id1 = GeneratorId::new(id_set.clone());
+        assert_eq!(id1.get(), 1);
+        let id2 = GeneratorId::new(id_set.clone());
+        assert_eq!(id2.get(), 2);
+        drop(id1);
+        assert!(id_set.lock().unwrap().iter().cloned().collect::<Vec<u64>>() == vec![0, 2]);
+        let id1 = GeneratorId::new(id_set.clone());
+        assert_eq!(id1.get(), 1);
     }
 }
